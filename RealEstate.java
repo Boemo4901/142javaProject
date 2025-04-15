cde71baa26598b6b41479ff9082f42226f461536
Boemo4901 <202404901@ub.ac.bw>
2025-04-15 20:36:53 +0200

    Update RealEstate.java

--- RealEstate.java
+++ RealEstate.java
@@ -2,5 +2,15 @@ public class RealEstate {
     private String city;
     private String suburb;
     private int bedrooms;
+    private int price;
+    private int rating;
+
+    // creating constructors
+    
+
+    //getters and setters
+    
+
+    //print out/return 
    
 }
