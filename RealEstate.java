540669b8852e50358f5e3167d4980c9ecd88fa54
Koketso169 <202204027@ub.ac.bw>
2025-04-16 12:25:00 +0200

    Update RealEstate.java
    adding getters.

--- RealEstate.java
+++ RealEstate.java
@@ -2,15 +2,23 @@ public class RealEstate {
     private String city;
     private String suburb;
     private int bedrooms;
-    private int price;
+    private double price;
     private int rating;
 
     // creating constructors
     
 
     //getters and setters
-    
+    public String getCity(){
+        return city;
+    }
 
+    public String getsuburb(){
+        return suburb;
+
+    }
+
+    public  int getBedroom(){
+
+        
     //print out/return 
-   
-}
