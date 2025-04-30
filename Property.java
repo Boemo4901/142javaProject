404dd37a4d173cef94878b7c98c4e2aed2f077a5
Karabo-M153 <202400841@ub.ac.bw>
2025-04-30 13:37:26 +0200

    added comments

--- Property.java
+++ Property.java
@@ -1,8 +1,11 @@
+/*Interface defining the core behaviours of a property
+ *It retrieves the property's adress,price and rating.
+*/
+
 public interface Property{
-  
+
+  //Returns the full adress, the price and the rating of the property.
   String geFullAdress ();
   double getPrice ();
   int getRating ();
-
-
 }
