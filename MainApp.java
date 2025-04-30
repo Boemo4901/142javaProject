b76d844ed4e7eadcb7dc0659fdb0457f6406ad5f
Karabo-M153 <202400841@ub.ac.bw>
2025-04-30 12:53:34 +0200

    fixed minor errors

--- MainApp.java
+++ MainApp.java
@@ -1,13 +1,15 @@
 public class MainApp{
+    //Main application demonstrates the use of RealEstate and Apartment classes with sorting and searching
+    
     public static void main(String[] args){
         try{
             //listings includes houses and apartments
             RealEstate[] listings = new RealEstate[6];
             listings[0] = new RealEstate("Gaborone", "Tsholofelo East", 4, 5900, 4);
-            listings[1] = new RealEstate("Gaborone", "Bloack 7", 3, 7600, 5);
-            listings[2] = new Apartment("Jwaneng", "Pilane", 2, 6500, 2, 2, True);
+            listings[1] = new RealEstate("Gaborone", "Block 7", 3, 7600, 5);
+            listings[2] = new Apartment("Jwaneng", "Pilane", 2, 6500, 2, 2);
             listings[3] = new RealEstate("Gaborone", "Tsholofelo East", 3, 10500, 6);
-            listings[4] = new Apartment("Francistown", "Bloack 9", 3, 5000, 4, 3, False);
+            listings[4] = new Apartment("Francistown", "Bloack 9", 3, 5000, 4, 3);
             listings[5] = new RealEstate("Molepolole", "Maokane", 5, 12000, 8);
         
 
@@ -20,20 +22,19 @@ public class MainApp{
             displayListings(listings);
 
             //sort by rating 
-            // Sort by rating
             RealEstateManager.insertionSortByRating(listings);
             System.out.println("\n Listings Sorted by Rating:");
             displayListings(listings);
+
+            //Linear search by Suburb
+            String targetSuburb = "Block 7";
+            int linearIndex = RealEstateManager.linearSearchBySuburb(listings, targetSuburb);
+            System.out.println("\nLinear Search for Suburb " + targetSuburb + ":");
+            if(linearIndex != -1) {
+                System.out.println("House found in " + listings[linearIndex]);
+            } else {
+                System.out.println(" Not found.");
+            }
         }
     }
-
-   //Linear search by Suburb
-   String targetSuburb = "Block 7";
-   int linearIndex = RealEstateManager.linearSearchBySuburb(listings, targetSuburb);
-   System.out.println("\n Linear Search for Suburb " + targetSuburb + ":");
-   if(linearIndex != -1) {
-       System.out.println("house found in " + listings[linearIndex]);
-   } else {
-       System.out.println(" Not found.");
-   }
 }
