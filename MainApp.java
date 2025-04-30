a09718211180750c0fd9ed4f27e98513a8b5e362
Karabo-M153 <202400841@ub.ac.bw>
2025-04-30 07:44:14 +0200

    edited the linear search

--- MainApp.java
+++ MainApp.java
@@ -2,8 +2,8 @@ public class MainApp{
 
    //Linear search by Suburb
    String targetSuburb = "Block 7";
-   int linearIndex = RealEstate.linearSearchBySuburb(listings, targetSuburb);
-   System.out.println("\n Linear Search by Suburb " + targetSuburb + ":");
+   int linearIndex = RealEstateManager.linearSearchBySuburb(listings, targetSuburb);
+   System.out.println("\n Linear Search for Suburb " + targetSuburb + ":");
    if(linearIndex != -1) {
        System.out.println("house found in " + listings[linearIndex]);
    } else {
