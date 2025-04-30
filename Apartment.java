eb89e0d234c61706458cfaacbe8094ac792a7b4a
Karabo-M153 <202400841@ub.ac.bw>
2025-04-30 23:13:58 +0200

    added comments

--- Apartment.java
+++ Apartment.java
@@ -1,14 +1,22 @@
+/*Class representing an Apartment, which is a specialized type of RealEstate
+ * Included additional apartment-specific attributes such a floor level.
+ */
+
 public class Apartment extends RealEstate{
   private int floorLevel;
 
-  public Apartment (String city, String suburb, int bedrooms,Int price,Int rating,Int floorLevel )throws invalidRatingException {
-    super( city, suburb ,bedrooms ,price,rating );
+  public Apartment (String city, String suburb, int bedrooms,double price,int rating,int floorLevel ) throws InvalidRatingException {
+    super( city, suburb, bedrooms, price, rating );
+    this.floorLevel = floorLevel;
+
   }
-  public int getFloorLevel(){
+  public int getFloorLevel() {
     return floorLevel;
-      }
-  @overide
+  }
+
+  // Returns a formatted string representation of the apartment.
+  @Override
   public String toString(){
-    return super.tostring()+", located on floor "+floorLevel;
+    return super.toString() + ", located on floor " + floorLevel;
   }
 }
